c96bb38ba13c9a0a14e59ae50ab35e77411b1c6d
duezt <66556666+duezt@users.noreply.github.com>
2022-04-12 21:22:43 -0400

    Instalacion NPM

---
+++ Clase_3/elseif.js
@@ -0,0 +1,11 @@
+
+let edad = 19;
+
+if(edad >= 18 && edad <= 21) {
+    console.log("Aun no eres mayor de edad en los estados unidos ");
+
+} else if(edad > 14 && edad < 18){
+        console.log("Aun no eres mayor de edad en los estados unidos")
+} else {
+    console.log("Eres mayor de edad");
+}
