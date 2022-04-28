691d5a40cf98889097b966b80b393008fef210f1
duezt <66556666+duezt@users.noreply.github.com>
2022-04-28 18:31:17 -0400

    Update

--- Clase_5/index.js
+++ Clase_5/index.js
@@ -1,13 +1,9 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-app.use(express.static(__dirname   +'assets'))
-
-app.get("/mi-primer-html",function(req, res){
-res.sendFile(__dirname + "/index.html")
+app.use(express.static('assets'));
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
 })
 
-
-app.listen(3000);
-console.log("Express esta corriendo en el puerto: 3000");
-console.log("htmp://localhost:3000");
+app.listen(3000)
